e0318f9d93f49e4250c196c686b1596449fe0f21
Dan Herrera <whoahbot@gmail.com>
2020-01-01 16:55:10 -0800

    Staging changes before working on dockerfile.

--- sonumator.py
+++ sonumator.py
@@ -10,8 +10,9 @@ from subcommands.create_samples import create_samples
       
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Sonumator")
-    parser.add_argument('--path', help='the path to the .wav files to be processed')
-    parser.add_argument('--csv', help='the .csv file that contains the start and end times for sampling')
+    parser.add_argument('--path', required=True, help='the path to the .wav files to be processed')
+    parser.add_argument('--csv', required=True, help='the .csv file that contains the start and end times for sampling')
+    parser.add_argument('--output', default="output/", help='the output directory to write files to')
 
     args = parser.parse_args()
     create_samples(args)
--- subcommands/create_samples.py
+++ subcommands/create_samples.py
@@ -1,31 +1,54 @@
 import math
+import os
 import glob
 
 import librosa
 import pandas as pd
 import soundfile as sf
 
-def make_sample(file_list, start_time, end_time):
+def between(start, end):
+    if start <= number <= end:
+        return True
+    else:
+        return False
+
+def make_sample(file_list, start_time, end_time, padding=0):
     """
     Each file in `file_list` is 4 hours long, calculate the offset in the file
-    based on the relative time. The time index for each file was off by 6 seconds
-    so add that for each file by index too.
+    based on the relative time.
     """
-    index = math.floor(start_time/14400)
-      
-    indexed_start_time = start_time - (index * 14400) + (index * 6)
-    indexed_end_time = end_time - (index * 14400) + (index * 6)
+    index = math.floor(start_time / 14400)
 
-    return librosa.load(file_list[index], offset=indexed_start_time, sr=5000, duration=indexed_end_time - indexed_start_time)
+    indexed_start_time = start_time - (index * 14400)
+    indexed_end_time = end_time - (index * 14400) + padding
+
+    return librosa.load(
+        file_list[index],
+        offset=indexed_start_time,
+        sr=5000,
+        duration=indexed_end_time - indexed_start_time,
+    )
+
+
+def create_damselfish_samples(df, file_list, output):
+    os.mkdir(output)
+    for i in range(0, len(df)):
+        row = df.iloc[i]
+        sample, sr = make_sample(file_list, row[0], row[1], .5)
+        sf.write(f"{output}/{i}.wav", sample, sr)
+
+
+def create_noise_samples(df, file_list, output):
+    os.mkdir(output)
+    for i in range(0, len(df)):
+        sample, sr = make_sample(file_list, start_time + i, start_time + i + 1, .5)
+        sf.write(f"{output}/{i}.wav", sample, sr)
 
 
 def create_samples(args):
-    df = pd.read_csv(args.csv, usecols=[3,4])
+    df = pd.read_csv(args.csv, usecols=[3, 4])
     file_list = glob.glob(f"{args.path}/*.wav")
     file_list.sort()
 
-    for i in range(0, 100):
-        row = df.iloc[i]
-        sample, sr = make_sample(file_list, row[0], row[1])
-        sf.write(f"{args.output}/{i}.wav", sample, sr)
-    
+    create_damselfish_samples(df, file_list, f"{args.output}/damselfish")
+    create_noise_samples(df, file_list, f"{args.output}/noise")
