9f642c33d433a44f2ae10440f6bf0925ace1efe8
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-03-03 01:52:46 +0000

    Bin times to the right.
    - Add offset to process all files.

--- process_all_files.sh
+++ process_all_files.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-for file in $(gsutil ls gs://sonumator/recordings/2015-2016/*.wav); do
+args=("$@")
+
+files=$(gsutil ls gs://sonumator/recordings/2015-2016/*.wav | tail -q -n +${args[0]});
+
+for file in $files; do
     echo "Processing $file"
     python ./sonumator.py classify_file --file $file --training-set ./output/ --output ./csv/
 done
