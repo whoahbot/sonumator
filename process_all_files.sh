f4efbe74eaf76efde3eace23ff4415a4535099d7
Dan Herrera <whoahbot@gmail.com>
2020-03-15 16:13:51 +0000

    Modify process script
    - Only process files that don't exist as csv files in the output dir.

--- process_all_files.sh
+++ process_all_files.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-set -e
+set -o pipefail
 
 args=("$@")
 
-files=$(gsutil ls gs://sonumator/recordings/2015-2016/*.wav | tail -q -n +${args[0]});
+input_files=$(gsutil ls gs://sonumator/recordings/${args[0]}/*.wav);
 
-for file in $files; do
-    echo "Processing $file"
-    time python ./sonumator.py classify_file --file $file --training-set ./output/ --output ./csv/
+trimmed_input_files=("${input_files[@]%.*}")
+trimmed_output_files=("${output_files[@]%.*}")
+
+for file in $input_files; do
+    base_file_name=$(basename ${file[@]%.*})
+    file_exists=$(gsutil ls gs://sonumator/csv/${base_file_name}.csv); rc=$?
+    if [ $rc == 1 ]
+    then 
+        echo "Processing $file"
+        time python ./sonumator.py classify_file --file $file --training-set ./output/ --output ./csv/
+    fi
 done
